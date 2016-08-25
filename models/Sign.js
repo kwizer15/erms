9fdba7146f1a67d9b643fe8b64dd3707a147cff5
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2016-08-25 17:41:24 +0200

    Remove old code commented

--- models/Sign.js
+++ models/Sign.js
@@ -7,20 +7,4 @@ const schema = new mongoose.Schema({
   updated_at: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Sign', schema);;
-
-// test
-
-// var sign = new Sign({name:'John Doe', phone:'0123456789'});
-// sign.save(function(err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(sign);
-//   }
-// });
-
-// Sign.find((err, signs) => {
-//   if (err) console.error(err)
-//   else console.log(signs);
-// });
+module.exports = mongoose.model('Sign', schema);
